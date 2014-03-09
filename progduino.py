1f8c7a2980a57dacd83adfec357e8a35f39877c8
marlonjke <marlonjke@gmail.com>
2014-03-08 23:47:40 -0300

    [UPD] Configurações na tela

--- progduino.py
+++ progduino.py
@@ -1,18 +1,24 @@
 #! /usr/bin/env python
 #-*- encoding:utf-8 -*-
-
-
 import gtk
 import pygtk
 pygtk.require('2.0')
 import serial
 
+port = '/dev/ttyACM0'
+baud_rate = 9600
+
+try:
+    com = serial.Serial(port, baud_rate)
+except:
+    print 'Dispositivo não conectado'
 
 class Progduino:
     def __init__(self):
-        self.janela = gtk.Window()
 
-        self.janela.set_title("ProgDuino - 0.1")
+
+        self.janela = gtk.Window()
+        self.janela.set_title("The Wakemanduino - 0.1")
         self.janela.set_position(gtk.WIN_POS_CENTER)
         self.janela.set_border_width(15)
         self.janela.connect('destroy', lambda w: gtk.main_quit())
@@ -20,48 +26,73 @@ class Progduino:
 
         #Box para Modificação/ Envio de acordes
         self.boxChord = gtk.HBox(False, 1)
+
+        self.btnSend = gtk.Button("Send")
+        self.btnSend.connect('clicked', self.on_send_chord_clicked)
+        self.boxChord.pack_start(self.btnSend)
+
         self.chordChange = gtk.combo_box_entry_new_text()
         self.chordChange.append_text('')
         self.boxChord.pack_start(self.chordChange)
-        chords = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
-        for chord in chords:
+
+        self.chords = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+        for chord in self.chords:
             self.chordChange.append_text(chord)
+
         self.chordChange.set_active(1)
 
         self.typeChordChange = gtk.combo_box_entry_new_text()
         self.typeChordChange.append_text('')
         self.boxChord.pack_start(self.typeChordChange)
-        typeChords = ['Major', 'Minor', 'Diminuto']
-        for typeChord in typeChords:
-            self.typeChordChange.append_text(typeChord)
-        self.typeChordChange.set_active(1)
 
-        self.btnSend = gtk.Button("Send Chord")
-        self.btnSend.connect('clicked', self.on_send_chord_clicked)
-        self.boxChord.pack_start(self.btnSend)
+        self.typeChords = ['Major', 'Minor', 'Diminuto']
+        for typeChord in self.typeChords:
+            self.typeChordChange.append_text(typeChord)
+
+        self.typeChordChange.set_active(1)
         self.conteudo.pack_start(self.boxChord)
 
         #Box com outras configurações.
-        self.confChord = gtk.VBox(False, 1)
+        self.confChord = gtk.HBox(False, 1)
 
-        self.button1 = gtk.RadioButton(None, "radio button1")
-        # button1.connect("toggled", self.callback, "radio button 1")
-        self.confChord.pack_start(self.button1, gtk.TRUE, gtk.TRUE, 0)
-        self.button1.show()
+        self.onoff = False
+        self.btOnOff = gtk.Button('off')
+        self.btOnOff.set_size_request(37, 25)
+        self.btOnOff.connect('clicked', self.on_onoff_clicked)
+        self.confChord.pack_start(self.btOnOff, False, False, 0)
 
-        self.button2 = gtk.RadioButton(None, "radio button2")
-        # button2.connect("toggled", self.callback, "radio button 2")
-        self.button2.set_active(gtk.TRUE)
-        self.confChord.pack_start(self.button2, gtk.TRUE, gtk.TRUE, 0)
-        self.button2.show()
+        self.actualChord = gtk.Label()
+        self.set_actual_chord()
+        self.confChord.pack_start(self.actualChord, False, False, 0)
 
         self.conteudo.pack_start(self.confChord)
-
         self.janela.add(self.conteudo)
         self.janela.show_all()
 
+    def set_actual_chord(self):
+        data = str(self.chords[self.chordChange.get_active() - 1]) + ' ' + str(self.typeChords[self.typeChordChange.get_active() -1])
+        self.actualChord.set_label('Actual Chord: ' + data)
+
     def on_send_chord_clicked(self, *args):
-        print 'Teste Acorde', self.chordChange.get_active(), self.typeChordChange.get_active()
+        data = str(self.chords[self.chordChange.get_active() - 1]) + ' ' + str(self.typeChords[self.typeChordChange.get_active() -1])
+        print data
+        self.set_actual_chord()
+        self.send_data(data)
+
+
+    def on_onoff_clicked(self, *args):
+        self.onoff = not self.onoff
+
+        self.send_data(self.onoff)
+
+        if self.onoff:
+            self.btOnOff.set_label('on')
+        else:
+            self.btOnOff.set_label('off')
+
+    def send_data(self, data):
+        # com.write(data)
+        print data
 
 if __name__ == '__main__':
     app = Progduino()
